c739db10b0714d9b2bb20dde1f3d24183a312871
Rostyk Lukavyi <rostyk.lukavyi842@gmail.com>
2021-07-12 22:28:23 +0300

    add selectors

---
+++ src/features/selectors.js
@@ -0,0 +1,7 @@
+export default {
+  getPosts: state => state.posts,
+  getPopup: state => state.popup,
+  getAuth: state => state.auth,
+  getLogin: state => state.login,
+  getSignup: state => state.signup,
+};
